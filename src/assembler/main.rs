028b5afd4ab72424fa3b65bc49efa19e8048ab9e
Lukas Wagner <lwagner94@posteo.at>
2018-11-11 21:16:58 +0100

    Implemented tokenizer for assembler

--- src/assembler/main.rs
+++ src/assembler/main.rs
@@ -1,4 +1,26 @@
+#![allow(dead_code)]
+
+extern crate regex;
+#[macro_use] extern crate failure;
+
+mod parser;
+mod tokenizer;
+
+use regex::Regex;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::prelude::*;
+use std::env;
+
+use failure::Error;
+
 
 fn main() {
-    print!("Hello");
+    let args: Vec<String> = env::args().collect();
+    let mut reader = BufReader::new(File::open(&args[1]).unwrap());
+    let tokens = tokenizer::tokenize(&mut reader);
+
+    println!("{:#?}", tokens);
 }
+
---
+++ src/assembler/tokenizer.rs
@@ -0,0 +1,125 @@
+use std::io::BufRead;
+
+
+#[derive(Debug, PartialOrd, PartialEq)]
+pub struct Line {
+    line_number: usize,
+    text: String,
+}
+
+#[derive(Debug)]
+pub struct Token {
+    token: String,
+    position: usize
+}
+
+#[derive(Debug)]
+pub struct TokenizedLine {
+    line: Line,
+    tokens: Vec<Token>
+}
+
+fn strip_comments(line: String) -> String {
+    match line.find("//") {
+        Some(location) => line[..location].into(),
+        None => line
+    }
+}
+
+fn trim(line: String) -> String {
+    line.trim().into()
+}
+
+fn split_lines(reader: &mut BufRead) -> Vec<Line> {
+    let mut result = Vec::new();
+
+    for (mut line_number, text_result) in reader.lines().enumerate() {
+        let text = text_result.unwrap();
+
+        line_number += 1;
+
+        result.push(Line {
+            line_number,
+            text
+        })
+    }
+
+    result
+}
+
+fn tokenize_line(text: &str) -> Vec<Token> {
+    text.split_whitespace().map(|s| Token {
+        token: s.into(),
+        position: 0,
+    }).collect()
+}
+
+pub fn tokenize(reader: &mut BufRead) -> Vec<TokenizedLine> {
+    let lines = split_lines(reader);
+
+    let mut tokenized_lines = Vec::new();
+
+    for line in lines {
+        let text = trim(strip_comments(line.text.clone()));
+        let line_number = line.line_number;
+        let tokens: Vec<Token> = tokenize_line(&text);
+
+        tokenized_lines.push(TokenizedLine {
+            line,
+            tokens
+        })
+    }
+
+    tokenized_lines
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_strip_comments_no_comment() {
+        let s = "foobar";
+        assert_eq!(&strip_comments(s.into()), s);
+    }
+
+    #[test]
+    fn test_strip_comments_comment() {
+        let s = "foobar // test";
+        assert_eq!(&strip_comments(s.into()), "foobar ");
+    }
+
+    #[test]
+    fn test_trim_no_change() {
+        let s = "foobar";
+        assert_eq!(&trim(s.into()), s);
+    }
+
+    #[test]
+    fn test_trim() {
+        let s = "  foobar   \t";
+        assert_eq!(&trim(s.into()), "foobar");
+    }
+
+    #[test]
+    fn test_split_lines_empty() {
+        let mut s = Cursor::new("abc\n\nghi");
+        let res = split_lines(&mut s);
+
+        assert_eq!(res, vec![
+            Line {
+                line_number: 1,
+                text: "abc".into()
+            },
+            Line {
+                line_number: 2,
+                text: "".into()
+            },
+            Line {
+                line_number: 3,
+                text: "ghi".into()
+            }]);
+    }
+}
